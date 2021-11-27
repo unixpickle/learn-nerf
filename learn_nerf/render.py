8c0232218322c2fe9affa9f67e4a31a5a65c3b2f
Alex Nichol <unixpickle@gmail.com>
2021-11-27 14:14:40 -0500

    setup inverse transform sampling

--- learn_nerf/render.py
+++ learn_nerf/render.py
@@ -1,32 +1,38 @@
+from typing import Tuple
+
 import jax
 import jax.numpy as jnp
 
 
 def stratified_sampling(
-    key: jax.random.PRNGKey,
-    count: int,
     batch_size: int,
-    min: jnp.ndarray,
-    max: jnp.ndarray,
+    count: int,
+    key: jax.random.PRNGKey,
+    t_min: jnp.ndarray,
+    t_max: jnp.ndarray,
 ) -> jnp.ndarray:
     """
-    :param key: RNG key for sampling.
-    :param count: number of samples per batch element.
     :param batch_size: number of batch elements.
-    :param min: a scalar array containing the minimum value.
-    :param max: a scalar array containing the maximum value.
+    :param count: number of samples per batch element.
+    :param key: RNG key for sampling.
+    :param t_min: a scalar array containing the minimum value.
+    :param t_max: a scalar array containing the maximum value.
     :return: a [batch_size x count] batch of t values, where
              min <= t <= max and t_i < t_{i+1} for all i for
              each batch element.
     """
-    bin_size = (max - min) / count
-    midpoints = jnp.arange(0, count, dtype=jnp.float32) * bin_size + min
+    bin_size = (t_max - t_min) / count
+    midpoints = jnp.arange(0, count, dtype=jnp.float32) * bin_size + t_min
     randoms = jax.random.uniform(key, (batch_size, count), maxval=bin_size)
     return randoms + midpoints
 
 
 def render_ray(
-    ts: jnp.ndarray, t_min: jnp.ndarray, densities: jnp.ndarray, rgbs: jnp.ndarray
+    ts: jnp.ndarray,
+    t_min: jnp.ndarray,
+    densities: jnp.ndarray,
+    rgbs: jnp.ndarray,
+    background: jnp.ndarray,
 ) -> jnp.ndarray:
     """
     Perform volumetric rendering given density and color samples along a ray.
@@ -35,11 +41,71 @@ def render_ray(
     :param t_min: a scalar array containing the minimum t value.
     :param densities: an [N x T] batch of non-negative density outputs.
     :param rgbs: an [N x T x 3] batch of RGB values.
+    :param background: an RGB background color, of shape [3].
     :return: an [N x 3] batch of RGB values.
     """
+    w, bg_prob = _termination_probabilities(ts, t_min, densities)
+    return jnp.sum(w[:, :, None] * rgbs, axis=1) + bg_prob * background
+
+
+def fine_sampling(
+    count: int,
+    key: jax.numpy.PRNGKey,
+    ts: jnp.ndarray,
+    t_min: jnp.ndarray,
+    t_max: jnp.ndarray,
+    densities: jnp.ndarray,
+    combine: bool = True,
+) -> jnp.ndarray:
+    """
+    Sample points along a ray leveraging density information from a coarsely
+    sampled ray.
+
+    :param count: the number of points to sample.
+    :param key: the RNG key to use for sampling.
+    :param ts: the sampled ts from the coarse ray.
+    :param t_min: the minimum t value to sample.
+    :param t_max: the maximum t value to sample.
+    :param densities: the sampled non-negative densities for ts.
+    :param combine: if True, combine the new sampled points with the old
+                    sampled points in one sorted array.
+    :return: an [N x T'] array of sampled ts, similar to stratified_sampling().
+    """
+    w, bg_prob = _termination_probabilities(ts, t_min, densities)
+    w = jnp.concatenate([w, bg_prob], axis=1)
+
+    # Setup an inverse CDF for inverse transform sampling.
+    xs = jnp.cumsum(w, axis=1)
+    xs = jnp.concatenate([jnp.zeros_like(xs[:, :1]), xs], axis=1)
+    xs = xs / xs[:, -1:]  # normalize
+    ys = jnp.concatenate(
+        [jnp.zeros_like(ts[:, :1]) + t_min, ts, jnp.zeros_like(ts[:, :1]) + t_max],
+        axis=1,
+    )
+
+    # Evaluate the inverse CDF at quasi-random points.
+    input_samples = stratified_sampling(
+        batch_size=ts.shape[0],
+        count=count,
+        key=key,
+        t_min=jnp.array(0.0),
+        t_max=jnp.array(1.0),
+    )
+    new_ts = jax.vmap(jnp.interp)(input_samples, xs, ys)
+
+    if combine:
+        combined = jnp.concatenate([ts, new_ts], axis=1)
+        return jnp.sort(combined, axis=1)
+    else:
+        return new_ts
+
+
+def _termination_probabilities(
+    ts: jnp.ndarray, t_min: jnp.ndarray, densities: jnp.ndarray
+) -> Tuple[jnp.ndarray, jnp.ndarray]:
     # Compute density*dt (the integrand)
     delta = ts - jnp.concatenate(
-        [jnp.tile(t_min.reshape([1, 1]), [ts.shape[0], 1]), ts[:, :-1]], axis=1
+        [jnp.zeros_like(ts[:, :1]) + t_min, ts[:, :-1]], axis=1
     )
     density_dt = delta * densities
 
@@ -51,7 +117,8 @@ def render_ray(
     )
     prob_survive = jnp.exp(-acc_densities_prev)
 
-    # Compute the probability of terminating at time t.
+    # Compute the probability of terminating at time t given
+    # that we made it to time t.
     prob_terminate = 1 - jnp.exp(-density_dt)
 
-    return jnp.sum((prob_survive * prob_terminate)[:, :, None] * rgbs, axis=1)
+    return prob_survive * prob_terminate, jnp.exp(-acc_densities_cur[:, -1:])
