fix bg, numerical stability, add stop gradient

learn_nerf/model.py
@@ -27,7 +27,7 @@ class NeRFModel(nn.Module):
             if i > 0:
                 z = nn.relu(z)
             z = nn.Dense(self.hidden_dim)(z)
-        density = jnp.log(1 + jnp.exp(nn.Dense(1)(z)))
+        density = nn.softplus(nn.Dense(1)(z))
         z = jnp.concatenate([z, d_emb], axis=-1)
         z = nn.relu(nn.Dense(self.color_layer_dim)(z))
         rgb = nn.tanh(nn.Dense(3)(z))

learn_nerf/scripts/train_nerf.py
@@ -56,7 +56,7 @@ def main():
         fine_ts=args.fine_samples,
     )
     step_fn = loop.step_fn(
-        jnp.array(t_min), jnp.array(t_max), jnp.array([0.0, 0.0, 0.0])
+        jnp.array(t_min), jnp.array(t_max), jnp.array([-1.0, -1.0, -1.0])
     )
 
     print("training...")

learn_nerf/train.py
@@ -114,7 +114,7 @@ class TrainLoop:
 
         # Evaluate the fine model using a combined set of points.
         fine_ts = coarse_ts.fine_sampling(
-            count=self.fine_ts, key=fine_key, densities=coarse_densities
+            count=self.fine_ts, key=fine_key, densities=jax.lax.stop_gradient(coarse_densities)
         )
         all_points = fine_ts.points(rays)
         direction_batch = jnp.tile(rays[:, 1:2], [1, all_points.shape[1], 1])