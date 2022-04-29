6e83fedc1a99c31ee7a883d54b469b7366f2f473
Alex Nichol <unixpickle@gmail.com>
2022-04-29 16:58:48 -0400

    fix point clouds, save color

--- point_cloud/main.go
+++ point_cloud/main.go
@@ -4,17 +4,21 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"image/color"
 	"image/png"
 	"log"
+	"math"
 	"math/rand"
 	"os"
 	"path/filepath"
 
 	"github.com/unixpickle/essentials"
 	"github.com/unixpickle/model3d/model3d"
+	"github.com/unixpickle/model3d/render3d"
+	"github.com/unixpickle/model3d/toolbox3d"
 )
 
 func main() {
@@ -29,7 +33,7 @@ func main() {
 	flag.Float64Var(&delta, "delta", 0.02, "marching cubes delta")
 	flag.IntVar(&maxPoints, "max-points", 50000, "maximum points to sample")
 	flag.StringVar(&dataDir, "data-dir", "", "data directory")
-	flag.StringVar(&outputPath, "output-path", "", "output STL path")
+	flag.StringVar(&outputPath, "output-path", "", "output zipped material OBJ path")
 	flag.Parse()
 	if dataDir == "" || outputPath == "" {
 		essentials.Die("Must specify -data-dir and -output-path")
@@ -37,9 +41,11 @@ func main() {
 
 	log.Println("Computing points...")
 	points := []model3d.Coord3D{}
+	colors := []render3d.Color{}
 	for i := 0; true; i++ {
 		metadataPath := filepath.Join(dataDir, fmt.Sprintf("%05d.json", i))
-		imgPath := filepath.Join(dataDir, fmt.Sprintf("%05d_depth.png", i))
+		depthPath := filepath.Join(dataDir, fmt.Sprintf("%05d_depth.png", i))
+		colorPath := filepath.Join(dataDir, fmt.Sprintf("%05d.png", i))
 
 		if _, err := os.Stat(metadataPath); os.IsNotExist(err) {
 			break
@@ -59,41 +65,35 @@ func main() {
 		essentials.Must(err)
 
 		origin := model3d.NewCoord3DArray(metadata.Origin)
-		xAxis := model3d.NewCoord3DArray(metadata.X)
-		yAxis := model3d.NewCoord3DArray(metadata.Y)
+		xAxis := model3d.NewCoord3DArray(metadata.X).Scale(math.Tan(metadata.XFov / 2))
+		yAxis := model3d.NewCoord3DArray(metadata.Y).Scale(math.Tan(metadata.YFov / 2))
 		zAxis := model3d.NewCoord3DArray(metadata.Z)
 
-		f, err = os.Open(imgPath)
+		err = ReadRGBD(depthPath, colorPath, func(x, y float64, depth uint16, c color.Color) {
+			zDist := (float64(depth) / 0xffff) * maxDepth
+			direction := zAxis.Add(xAxis.Scale(x)).Add(yAxis.Scale(y)).Normalize()
+			scale := zDist / direction.Dot(zAxis)
+			coord := origin.Add(direction.Scale(scale))
+			points = append(points, coord)
+			r, g, b, _ := color.RGBAModel.Convert(c).RGBA()
+			colors = append(colors, render3d.NewColorRGB(float64(r)/0xffff, float64(g)/0xffff, float64(b)/0xffff))
+		})
 		essentials.Must(err)
-		img, err := png.Decode(f)
-		f.Close()
-		essentials.Must(err)
-		for y := 0; y < img.Bounds().Dy(); y++ {
-			yFrac := 2*float64(y)/float64(img.Bounds().Dy()-1) - 1
-			for x := 0; x < img.Bounds().Dx(); x++ {
-				xFrac := 2*float64(x)/float64(img.Bounds().Dx()-1) - 1
-				brightness, _, _, _ := color.Gray16Model.Convert(img.At(x, y)).RGBA()
-				if brightness == 0xffff {
-					continue
-				}
-				zFrac := (float64(brightness) / 0xffff) * maxDepth
-				coord := origin.Add(zAxis.Scale(zFrac)).Add(xAxis.Scale(xFrac)).Add(yAxis.Scale(yFrac))
-				points = append(points, coord)
-			}
-		}
 	}
 
 	if len(points) > maxPoints {
 		log.Printf("Found %d points. Reducing to %d...", len(points), maxPoints)
 		rand.Shuffle(len(points), func(i, j int) {
 			points[i], points[j] = points[j], points[i]
+			colors[i], colors[j] = colors[j], colors[i]
 		})
 		points = points[:maxPoints]
+		colors = colors[:maxPoints]
 	} else {
 		log.Printf("Using all %d points.", len(points))
 	}
 
-	log.Println("Constructing solid...")
+	log.Println("Constructing solid and color function...")
 	min := points[0]
 	max := points[0]
 	for _, p := range points {
@@ -108,10 +108,60 @@ func main() {
 			return tree.Dist(c) < thickness
 		},
 	)
+	coordToColor := map[model3d.Coord3D]render3d.Color{}
+	for i, c := range points {
+		coordToColor[c] = colors[i]
+	}
+	colorFunc := toolbox3d.CoordColorFunc(func(c model3d.Coord3D) render3d.Color {
+		return coordToColor[tree.NearestNeighbor(c)]
+	})
 
 	log.Println("Creating mesh...")
 	mesh := model3d.MarchingCubesSearch(solid, delta, 8)
 
 	log.Println("Saving mesh...")
-	mesh.SaveGroupedSTL(outputPath)
+	mesh.SaveMaterialOBJ(outputPath, colorFunc.TriangleColor)
+}
+
+func ReadRGBD(depthPath, colorPath string, cb func(x, y float64, depth uint16, c color.Color)) error {
+	f, err := os.Open(depthPath)
+	if err != nil {
+		return err
+	}
+	depthImg, err := png.Decode(f)
+	f.Close()
+	if err != nil {
+		return err
+	}
+
+	f, err = os.Open(colorPath)
+	if err != nil {
+		return err
+	}
+	colorImg, err := png.Decode(f)
+	f.Close()
+	if err != nil {
+		return err
+	}
+
+	b := depthImg.Bounds()
+	if b != colorImg.Bounds() {
+		return errors.New("mismatched size of RGB and depth images")
+	}
+
+	for y := 0; y < b.Dy(); y++ {
+		yFrac := 2*float64(y)/float64(b.Dy()-1) - 1
+		for x := 0; x < b.Dx(); x++ {
+			xFrac := 2*float64(x)/float64(b.Dx()-1) - 1
+
+			depth, _, _, _ := color.Gray16Model.Convert(depthImg.At(x, y)).RGBA()
+			if depth == 0xffff {
+				continue
+			}
+			c := colorImg.At(x, y)
+			cb(xFrac, yFrac, uint16(depth), c)
+		}
+	}
+
+	return nil
 }
